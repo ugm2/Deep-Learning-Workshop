12b6346881bd3b97ca9394d799e0312ad8e18844
ugmSorcero <ugm2@alu.ua.es>
2021-08-23 20:44:06 +0000

    New dataset. Trying to debug nn not learning properly

---
+++ NeuralNetwork/examples/cat_vs_noncat.py
@@ -0,0 +1,64 @@
+from NeuralNetwork.nn_coursera import L_layer_model, predict
+import numpy as np
+import h5py
+from NeuralNetwork.neural_network import NeuralNetwork
+
+np.random.seed(1)
+
+def load_data():
+    train_dataset = h5py.File('data/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('data/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+# Explore your dataset 
+m_train = train_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+m_test = test_x_orig.shape[0]
+
+print ("Number of training examples: " + str(m_train))
+print ("Number of testing examples: " + str(m_test))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_x_orig shape: " + str(train_x_orig.shape))
+print ("train_y shape: " + str(train_y.shape))
+print ("test_x_orig shape: " + str(test_x_orig.shape))
+print ("test_y shape: " + str(test_y.shape))
+
+# Reshape the training and test examples 
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+# Standardize data to have feature values between 0 and 1.
+train_x = train_x_flatten/255.
+test_x = test_x_flatten/255.
+
+print ("train_x's shape: " + str(train_x.shape))
+print ("test_x's shape: " + str(test_x.shape))
+
+# nn_model = NeuralNetwork(
+#     layers_dict={
+#         'layers': [train_x.shape[0], 20, 7, 5, 1],
+#         'activations': ['relu', 'relu', 'relu', 'sigmoid']
+#     },
+#     learning_rate=0.0075,
+#     num_iterations=1000,
+#     verbose=True
+# )
+# nn_model.fit(train_x, train_y)
+
+layers_dims = [12288, 20, 7, 5, 1] #  4-layer model
+parameters, costs = L_layer_model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
+
+# pred_train = predict(train_x, train_y, parameters)
--- NeuralNetwork/neural_network.py
+++ NeuralNetwork/neural_network.py
@@ -1,4 +1,26 @@
 import numpy as np
+from copy import deepcopy
+
+def relu_backward(dA, cache):
+    """
+    Implement the backward propagation for a single RELU unit.
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+    
+    Z = cache
+    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
+    
+    # When z <= 0, you should set dz to 0 as well. 
+    dZ[Z <= 0] = 0
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
 
 class NeuralNetwork:
 
@@ -30,10 +52,10 @@ class NeuralNetwork:
         # Sigmoid
         sigmoid = lambda x: 1 / (1 + np.exp(-x))
         self.activation_functions['sigmoid'] = sigmoid
-        self.backward_activation_functions['sigmoid'] = lambda x: sigmoid(x) * (1 - sigmoid(x))
+        self.backward_activation_functions['sigmoid'] = lambda dA, Z: dA * (sigmoid(Z) * (1 - sigmoid(Z)))
         # ReLU
         self.activation_functions['relu'] = lambda x: np.maximum(0, x)
-        self.backward_activation_functions['relu'] = lambda x: np.where(x > 0, 1, 0)
+        self.backward_activation_functions['relu'] = lambda _, Z: np.where(Z > 0, 1, 0)
         # TODO: Add more activation functions
 
         # Validate activations
@@ -65,6 +87,7 @@ class NeuralNetwork:
         A = X
         L = len(self.layers)
         for l in range(1, L):
+            self.parameters['A' + str(l-1)] = A
             W = self.parameters['W' + str(l)]
             b = self.parameters['b' + str(l)]
             Z = np.dot(W, A) + b
@@ -85,7 +108,8 @@ class NeuralNetwork:
             cost: A float value containing the cost
         """
         # TODO: Generalize and add more cost functions
-        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A)) / Y.shape[0]
+        cost = - (np.dot(Y, np.log(A).T) + np.dot((1 - Y), np.log(1 - A).T)) / Y.shape[1]
+        cost = np.squeeze(cost) 
         return cost
 
     def _backward(self, A, Y):
@@ -106,14 +130,15 @@ class NeuralNetwork:
         L = len(self.layers)
 
         for l in reversed(range(1, L)):
+            A_prev = self.parameters['A' + str(l-1)]
             W = self.parameters['W' + str(l)]
             Z = self.parameters['Z' + str(l)]
             activation_function = self.activations[l-1]
             backward_activation_function = self.backward_activation_functions[activation_function]
-            dZ = dA * backward_activation_function(Z)
+            dZ = backward_activation_function(dA, Z)
             dA = np.dot(W.T, dZ)
-            self.grads['dW' + str(l)] = np.dot(dZ, A.T) / Y.shape[0]
-            self.grads['db' + str(l)] = np.sum(dZ, axis=1, keepdims=True) / Y.shape[0]
+            self.grads['dW' + str(l)] = np.dot(dZ, A_prev.T) / A_prev.shape[1]
+            self.grads['db' + str(l)] = np.sum(dZ, axis=1, keepdims=True) / A_prev.shape[1]
 
     def _update_parameters(self):
         """
@@ -132,6 +157,7 @@ class NeuralNetwork:
             X: A numpy.ndarray with shape (nx, m) that contains the input data
             Y: A numpy.ndarray with shape (1, m) that contains the training labels
         """
+        
         for i in range(self.num_iterations):
             A = self._forward(X)
             cost = self._cost(Y, A)
@@ -139,6 +165,7 @@ class NeuralNetwork:
             self._update_parameters()
             if self.verbose and i % 100 == 0:
                 print('Iteration {}: Cost = {}'.format(i, cost))
+
 
         # Print accuracy if verbose
         if self.verbose:
@@ -170,7 +197,7 @@ class NeuralNetwork:
         """
         Y_prediction = self.predict(X)
         Y_prediction = np.where(Y_prediction > 0.5, 1, 0)
-        return np.sum(Y_prediction == Y) / Y.shape[0]
+        return np.sum(Y_prediction == Y) / Y.shape[1]
 
     # TODO: Save, Load
 
