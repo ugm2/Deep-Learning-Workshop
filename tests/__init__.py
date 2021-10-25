429af30a10d272fd50e83e08ae51fcc9b7dc6f22
ugmSorcero <unai@sorcero.com>
2021-10-25 08:25:55 +0000

    Starts creating unit tests

---
+++ tests/__init__.py
@@ -0,0 +1,15 @@
+'''
+Unit tests for DL Workshop
+'''
+
+import unittest
+
+import os
+import sys
+import logging
+
+sys.path.append(os.getcwd())
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
