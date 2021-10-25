3f39c3b158c075ae425dd6421cc663e20a8ac4b9
ugmSorcero <unai@sorcero.com>
2021-10-25 10:56:20 +0000

    Activation Functions tested

--- tests/__init__.py
+++ tests/__init__.py
@@ -14,10 +14,6 @@ assert ActivationFunctionTests
 
 sys.path.append(os.getcwd())
 
-def _log_test_title(title, logger):
-    line = "=" * len(title)
-    logger.info("\n\n%s\n%s\n%s" % (line, title, line))
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     unittest.main()
